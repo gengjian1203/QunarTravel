3c12fd5f9229f087b535530e56088bedf6fa9f42
gengjian1203 <gengjian1203@foxmail.com>
2019-08-17 19:16:45 +0800

    :tada: init frame

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 修复部分机型点击延迟300毫秒的bug
+import fastclick from 'fastclick'
+// 重置CSS样式
+import './assets/style/reset.css'
+// 修复1像素边框的bug
+import './assets/style/border.css'
 
 Vue.config.productionTip = false
+fastclick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="text">This is Home</div>
+    <router-link to="/list">列表</router-link>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home'
+
+}
+</script>
+
+<style scoped>
+  .text {
+    font-size: 20px;
+  }
+</style>
