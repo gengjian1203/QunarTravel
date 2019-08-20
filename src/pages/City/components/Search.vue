7e351b6ebe45d27f6b104216027c47c0a1412b28
gengjian1203 <gengjian1203@foxmail.com>
2019-08-20 20:27:13 +0800

    :sparkles: Add search model

---
+++ src/pages/City/components/Search.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="wrapper">
+    <div class="upper">
+      <ul class="address">
+        <li :class="{'address-child' : true, 'active' : bActive}" @click="HandleAddrInClick">境内</li>
+        <li :class="{'address-child' : true, 'active' : !bActive}" @click="HandleAddrOutClick">境外·港澳台</li>
+      </ul>
+    </div>
+    <div class="downer">
+      <input class="search-input" type="text" name="" v-model="strSearch" placeholder="请输入城市名或拼音"/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'CitySearch',
+  data () {
+    return {
+      bActive: true,
+      strSearch: ''
+    }
+  },
+  methods: {
+    HandleAddrInClick (el) {
+      this.bActive = true
+    },
+    HandleAddrOutClick (el) {
+      this.bActive = false
+    }
+  }
+}
+
+</script>
+
+<style lang="stylus" scoped>
+  @import "~style/varibles.styl"
+
+  .wrapper
+    background: $BGColor
+    .upper
+      width: 100%
+      height: .6rem
+      .address
+        width: 73%
+        height: .4rem
+        border: 1px solid #ffffff
+        border-radius: .05rem
+        color: #ffffff
+        background: $BGColor
+        margin: 0 auto
+      .active
+        color: $BGColor
+        background: #ffffff
+      .address-child
+        float: left
+        width: 50%
+        height: 100%
+        line-height: .4rem
+        text-align: center
+    .downer
+      width: 100%
+      height: .8rem
+      line-height: .8rem
+      text-align: center
+      .search-input
+        box-sizing: border-box
+        width: 95%
+        height: .6rem
+        text-align: center
+        border-radius: $borderRadius
+        padding: 0 .2rem
+</style>
--- src/pages/Home/components/Header.vue
+++ src/pages/Home/components/Header.vue
@@ -29,35 +29,35 @@ export default {
 <style lang="stylus" scoped>
   @import '~style/varibles.styl'
   .home
-    display: flex;
-    height: $headerHeight;
-    line-height: $headerHeight;
-    color: #ffffff;
-    background: $BGColor;
+    display: flex
+    height: $headerHeight
+    line-height: $headerHeight
+    color: #ffffff
+    background: $BGColor
     .home-left
-      float: left;
-      margin: 0px .1rem;
+      float: left
+      margin: 0px .1rem
       .icon-back
-        font-size: .5rem;
+        font-size: .5rem
     .home-input
-      flex: 1;
-      height: .6rem;
-      line-height: .59rem;
-      margin: auto .1rem;
-      padding: 0px .15rem;
-      color: #dddddd;
-      background: #ffffff;
-      border-radius: 0.08rem;
+      flex: 1
+      height: .6rem
+      line-height: .59rem
+      margin: auto .1rem
+      padding: 0px .15rem
+      color: #dddddd
+      background: #ffffff
+      border-radius: $borderRadius
       .icon-input
-        margin: 0 .03rem;
+        margin: 0 .03rem
     .home-right
-      float: right;
-      margin-left: .1rem;
+      float: right
+      margin-left: .1rem
       margin-right: .28rem
       color: #ffffff
       .icon-arrow
-        margin-left: -.04rem;
-        margin-top: -2rem;
-        font-size: .24rem;
+        margin-left: -.04rem
+        margin-top: -2rem
+        font-size: .24rem
 
 </style>
