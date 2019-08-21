7c2905f353d6b0ad931451aa5f8c656065155253
gengjian1203 <gengjian1203@foxmail.com>
2019-08-21 11:37:02 +0800

    :sparkles: Add city search function

--- src/pages/City/components/Search.vue
+++ src/pages/City/components/Search.vue
@@ -7,19 +7,47 @@
       </ul>
     </div>
     <div class="downer">
-      <input class="search-input" type="text" name="" v-model="strSearch" placeholder="请输入城市名或拼音"/>
+      <input class="search-input"
+             type="text"
+             name=""
+             v-model="strSearch"
+             placeholder="请输入城市名或拼音"/>
+    </div>
+    <div class="search-wrapper" ref="scroll" v-show="strSearch">
+      <ul class="search-content">
+        <li class="search-item border-bottom"
+            v-for="(item, index) of listResult"
+            :key="index">
+            {{item}}
+        </li>
+        <li class="search-item border-bottom" v-show="bShowMatch">
+          没有找到符合要求的地名
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
 
+import BScroll from 'better-scroll'
+
 export default {
   name: 'CitySearch',
+  props: {
+    cities: Object
+  },
   data () {
     return {
       bActive: true,
-      strSearch: ''
+      strSearch: '',
+      listResult: [],
+      timer: null
+    }
+  },
+  computed: {
+    bShowMatch () {
+      return !this.listResult.length
     }
   },
   methods: {
@@ -29,6 +57,31 @@ export default {
     HandleAddrOutClick (el) {
       this.bActive = false
     }
+  },
+  watch: {
+    strSearch () {
+      // 函数节流
+      if (!this.timer) {
+        setTimeout(() => {
+          const list = []
+          for (let i in this.cities) {
+            this.cities[i].forEach((value) => {
+              if ((value.spell.indexOf(this.strSearch) > -1) ||
+                  (value.name.indexOf(this.strSearch) > -1)) {
+                list.push(value.name)
+              }
+            })
+          }
+          this.listResult = list
+          // 重置定时器
+          clearTimeout(this.timer)
+          this.timer = null
+        }, 100)
+      }
+    }
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.scroll)
   }
 }
 
@@ -36,6 +89,10 @@ export default {
 
 <style lang="stylus" scoped>
   @import "~style/varibles.styl"
+
+  .border-bottom
+    &:before
+      border-color: #aaaaaa
 
   .wrapper
     background: $BGColor
@@ -71,4 +128,19 @@ export default {
         text-align: center
         border-radius: $borderRadius
         padding: 0 .2rem
+    .search-wrapper
+      overflow: hidden
+      position: absolute
+      z-index: 1
+      width: 100%
+      top: 114px
+      left: 0
+      right: 0
+      bottom: 0
+      background: #ffffff
+      .search-content
+        .search-item
+          height: .68rem
+          line-height: .68rem
+          padding-left: .2rem
 </style>
